2ad57fb49c9cb38856ede7c86d519e361c78a4ea
Gabriel Campos <gabriel.borges.ferreira@gmail.com>
2019-08-26 23:21:51 -0300

    Criando repository e service para Categoria

--- src/main/java/com/gabrielcampos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gabrielcampos/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,36 @@
 package com.gabrielcampos.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielcampos.cursomc.domain.Categoria;
+import com.gabrielcampos.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2,"Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+
+	@Autowired
+	private CategoriaService service;
+
+	// o endpoint agora vai ser o /categorias/algumId
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+
+	// Para o spring saber que o id da url vai ir pro id da variável tem que inserir
+	// a anotação @PathVariable
+
+	// O tipo ResponseEntity é um tipo especial do spring que já encapsula várias
+	// informações de uma resposta http para um serviço REST
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+		Categoria obj = service.buscar(id);
+
+		// retorna um objeto ResponseEntity falando que está tudo ok e que tem como
+		// corpo o obj de categoria
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/gabrielcampos/cursomc/services/CategoriaService.java
@@ -0,0 +1,28 @@
+package com.gabrielcampos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielcampos.cursomc.domain.Categoria;
+import com.gabrielcampos.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	// Quando você cria uma dependencia com a anotaçao Autowired, essa dependência
+	// vai ser automaticamente instanciada pelo spring pelo mecanismo de injeção de
+	// dependencia ou inversão de controle
+	@Autowired
+	private CategoriaRepository repo;
+
+	// implementação do serviço que busca uma categoria
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+	}
+
+}
