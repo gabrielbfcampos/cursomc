2c632766bb43caf6ce3080c7fad5da487a98ef72
Gabriel Campos <gabriel.borges.ferreira@gmail.com>
2019-08-26 23:39:42 -0300

    Criando operação de instanciação

--- src/main/java/com/gabrielcampos/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/gabrielcampos/cursomc/repositories/CategoriaRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.gabrielcampos.cursomc.domain.Categoria;
 
+//A interface JpaRepository é capaz de acessar os dados com base em um tipo que a gente passa, 
+//no caso Categoria e tambem tem que se colocar o tipo do atributo identificador que no caso 
+//é o Integer
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
